ecce05cf4790e7df7016713fcc65f4fdc67f484a
Alexsander Patrick Ferreira Santos <alexsander.patrick@gmail.com>
2023-01-31 16:32:53 -0300

    Project 3 - completed
    small system to identify the average with two notes
    
    Learning visibility

--- Notas_Visibilidade/src/empresa/Main.java
+++ Notas_Visibilidade/src/empresa/Main.java
@@ -1,8 +1,11 @@
 package empresa;
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Project init");
-        Notas Jonas = new Notas();
-        Jonas.info();
+        System.out.println(ConsoleColors.GREEN +"\n"+"---------- Project init ----------" + "\n"+ ConsoleColors.RESET);
+        Notas Jonas = new Notas(6.0, 12.0);
+        Jonas.info(true,true);
+        Jonas.setNota2(8.5);
+        Jonas.results(false);
+        System.out.println(ConsoleColors.GREEN +"\n"+"---------- End ----------"+ConsoleColors.RESET);
     }
 }
--- Notas_Visibilidade/src/empresa/Notas.java
+++ Notas_Visibilidade/src/empresa/Notas.java
@@ -1,11 +1,45 @@
 package empresa;
 
+import javax.naming.spi.DirStateFactory.Result;
+
 public class Notas {
     private double nota1;
     private double nota2;
-    
-    void info(){
-        System.out.println(nota1);
-        System.out.println(nota2);
+    private boolean approved;
+
+    Notas(Double nota1, Double nota2){
+        setNota1(nota1);
+        setNota2(nota2);
+    }
+
+    void info(boolean showN1, boolean showN2){
+        System.out.println("------------------------------");
+        System.out.print("Nota");
+        if(showN1 & showN2){System.out.print("s: \n");}else{System.out.print(": \n");}
+        if(showN1){System.out.println("Nota 1: "+nota1);}
+        if(showN2){System.out.println("Nota 2: "+nota2);}
+        System.out.println("------------------------------\n");
+    }
+    void results(boolean showNotes){
+        double arithmetic_mean = (nota1+nota2)/2;
+        
+        if(showNotes){info(true, true);}
+        if(arithmetic_mean >=6){approved = true;}
+        System.out.println("Aluno "+(approved?"Aprovado!":"em Recuperação!")+"\nMédia: "+ arithmetic_mean);
+
+    }
+    public void setNota1(double nota1) {
+        if(nota1 >= 0 & nota1 <=10){
+            if(this.nota1==0.0){System.out.println("Nova Nota 1: "+nota1+"\n");}
+            this.nota1 = nota1;
+        }
+        else{System.out.println("Nota 1 inválida\n");}
+    }
+    public void setNota2(double nota2) {
+        if(nota2 >= 0 & nota2 <=10){
+            if(this.nota2==0.0){System.out.println("Nova Nota 2: "+nota2+"\n");};
+            this.nota2 = nota2;
+        }
+        else{System.out.println("Nota 2 inválida\n");}
     }
 }
